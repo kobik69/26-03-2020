63595d3ec43a8f1e9dae40a07adcf6b78ed293e0
kobi kahila <kobik69@gmail.com>
2020-03-26 01:49:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _26_03_2020
+{
+
+    public class Humen
+    {
+        public string name;
+
+        public Humen(string name  )
+        {
+            this.name = name;  
+        }
+    }
+
+    public class Person : Humen
+    {
+        public int age;
+
+        public Person(string name ):base (name)
+        {
+
+        }
+
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Person p = new Person("kobi");
+        Console.WriteLine(p);
+
+        Person p1 = new Person("koko", 99);
+
+            Console.WriteLine("+++++++++++++++{0} ", p);  
+            Console.WriteLine(" {0}  + {1}", p1.name, p.name);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//        Person p = new Person("danny", 18);
+//    }
+//}
+
+//public class Human
+//{
+//    public string name;
+//    public Human(string name)
+//    {
+//        this.name = name;
+//    }
+//}
+
+//public class Person : Human
+//{
+//    public int age;
+//    public Person(string name) : base(name)
+//    {
+
+//    }
+
+//    public Person(string name, int age) : this(name)
+//    {
+//        this.age = age;
+//    }
+//}
